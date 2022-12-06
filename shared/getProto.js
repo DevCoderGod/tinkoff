4fe7d42e809c4907a8a2dbee59d992d5983ecfbb
devcodergod <devcodergod@gmail.com>
2022-12-06 23:07:01 +0300

    add download repo:Tinkoff/investAPI, branch: main

---
+++ shared/getProto.js
@@ -0,0 +1,25 @@
+const https = require('https')
+const path = require('path')
+const  fs = require('fs')
+const admzip = require('adm-zip')
+
+const arch=path.join(__dirname, 'proto.zip')
+const file = fs.createWriteStream(arch)
+const url = 'https://codeload.github.com/Tinkoff/investAPI/zip/main'
+
+file.on('close',async ()=>{
+	const azip = new admzip(arch)
+	try {
+		console.log(' Распаковка ')
+		await azip.extractAllTo(__dirname,true)
+	} catch (e) { 
+		console.log('Ошибка распаковки: ',e)
+	}
+	fs.unlink(arch, e=> console.log(' Успех! '))
+})
+
+const req = https.get(url, res => res.pipe(file))
+	.on('error', e => console.error('Ошибка загрузки: ', e))
+
+console.log(' Загрузка ')
+req.end()
