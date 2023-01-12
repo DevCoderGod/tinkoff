41b0a069f217c3609bc9caadfde953268305c028
devcodergod <devcodergod@gmail.com>
2023-01-12 20:08:17 +0300

    getProto.js to mjs

--- shared/getProto.js
+++ shared/getProto.js
@@ -1,10 +1,14 @@
-const https = require('https')
-const path = require('path')
-const  fs = require('fs')
-const admzip = require('adm-zip')
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { get } from 'https'
+import { join } from 'path'
+import { createWriteStream, mkdir, unlink } from 'fs'
+import admzip from 'adm-zip'
 
-const arch=path.join(__dirname, 'proto.zip')
-const file = fs.createWriteStream(arch)
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const arch=join(__dirname, 'proto.zip')
+const file = createWriteStream(arch)
 const url = 'https://codeload.github.com/Tinkoff/investAPI/zip/main'
 
 file.on('close',async ()=>{
@@ -12,14 +16,14 @@ file.on('close',async ()=>{
 	try {
 		console.log(' Распаковка ')
 		await azip.extractAllTo(__dirname,true)
-		await fs.mkdir('./shared/tsproto',()=>{})
+		await mkdir('./tsproto',()=>{})//дир для компиляции
 	} catch (e) { 
 		console.log('Ошибка распаковки: ',e)
 	}
-	fs.unlink(arch, e=> console.log(' Успех! '))
+	unlink(arch, e=> console.log(' Успех! '))
 })
 
-const req = https.get(url, res => res.pipe(file))
+const req = get(url, res => res.pipe(file))
 	.on('error', e => console.error('Ошибка загрузки: ', e))
 
 console.log(' Загрузка ')
