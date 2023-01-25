69e08d7882b8efdfd85940b0ef9120e616eb2811
devcodergod <devcodergod@gmail.com>
2023-01-25 19:12:13 +0300

    add mongo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 import * as VARS from './globalVars.js'
 import { Router } from './Router/index.js'
 
-const app = express();
+const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-VARS.isDEV && console.log(' isDev ');
+VARS.isDEV && console.log(' isDev ')
 
 app.use(express.json())
 if(VARS.isPROD) app.use(express.static(join(__dirname, '../../client_react/build')))
@@ -19,4 +20,20 @@ app.use('*', (req, res) => {
 	res.status(404).json({ message: 'Page not found' })
 })
 
-app.listen(VARS.PORT, () => console.log(`Running on port ${VARS.PORT}`));
+const start = async() => {
+	try {
+		mongoose.set('strictQuery', false) //DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7.
+		await mongoose.connect(VARS.DB_URL)
+			.catch(err => {
+				console.log('DB error: ',err)
+				throw new Error("err")
+			})
+			.finally(() => console.log(' DB OK '))
+
+		app.listen(VARS.PORT, () => console.log(`Running on port ${VARS.PORT}`))
+	} catch (err) {
+		console.log('server error === ',err)
+	}
+}
+
+start()
