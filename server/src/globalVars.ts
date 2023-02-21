2a2df10e811cb896c1f7d415ccefa6c053dc246a
devcodergod <devcodergod@gmail.com>
2023-02-21 13:56:57 +0300

    auth

--- server/src/globalVars.ts
+++ server/src/globalVars.ts
@@ -2,3 +2,7 @@ export const PORT = 5000
 export const isPROD = process.env.NODE_ENV === 'prod'
 export const isDEV = process.env.NODE_ENV === 'dev'
 export const DB_URL = 'mongodb://127.0.0.1:27017/auth'
+export const aTokenSecret = 'aTokenSecret'
+export const rTokenSecret = 'rTokenSecret'
+export const aTokenExpired = 'PT5M' //ISO 8601 5 minutes
+export const rTokenExpired = 'P5D' //ISO 8601 5 days
