ab408028c8cb41af6320982489ff1f56ee169ec4
devcodergod <devcodergod@gmail.com>
2023-03-27 15:45:59 +0300

    add ws: a Node.js WebSocket library

--- server/src/Router/tinkoff.ts
+++ server/src/Router/tinkoff.ts
@@ -1,10 +1,10 @@
 import { Request, Response, Router } from 'express'
-import { IToken } from "@models"
 import { Worker } from 'worker_threads'
+import { IToken } from "@models"
 
 export const tinkoff = Router()
 
-tinkoff.post('/connect', async (request:Request<{},any,{token: IToken["value"] | null}>, response) => {
+tinkoff.post('/connect', async (request:Request<{},any,{token: IToken["value"] | null}>, response:Response) => {
 	try {
 		const token = request.body ? request.body.token : null
 		if(!token){ throw new Error(' bad TToken ')}
@@ -13,16 +13,24 @@ tinkoff.post('/connect', async (request:Request<{},any,{token: IToken["value"] |
 		const workerData ={token, port}
 		const worker = new Worker("../server/build/Worker/worker.js",{ workerData })
 
-		worker.on('message', v => {console.log('message v === ',v)})
-		worker.on('error', err => {console.log('error err === ',err)})
-		worker.on('exit', exitCode => {console.log('exit exitCode === ',exitCode)})
-
-		setTimeout(async()=>{
-			let code = await worker.terminate()
-			console.log('code === ',code)
-		},10000)
-
+		worker.on('message', m => messageHandler(worker, m))
+		worker.on('error', err => errorHandler(worker, err))
+		worker.on('exit', exitCode => exitHandler(worker, exitCode))
+		
 	} catch (err) {
 		response.status(500).json({message: ` ${err} `})
 	}
+
+	function messageHandler(worker: Worker,m: any): void {
+		if(m.port) response.status(200).json({port:m.port})
+		if(m === "exit") worker.postMessage("terminate")
+	}
+	
+	function errorHandler(worker: Worker, err: Error): void {
+		console.log('error err === ',err)
+	}
+
+	function exitHandler(worker: Worker, exitCode: number): void {
+		console.log('exit exitCode === ',exitCode)
+	}	
 })
--- server/src/Worker/worker.ts
+++ server/src/Worker/worker.ts
@@ -1,6 +1,25 @@
+import http from 'http'
 import { workerData, parentPort } from 'worker_threads'
+import { WebSocketServer } from 'ws'
 
-const timer = setInterval(()=>{
-	console.log('worker process.pid === ', process.pid)
-	console.log('worker process workerData === ', workerData)
-},3000)
+if(!parentPort) throw new Error(`!parentPort`)
+if(!workerData) throw new Error(`!workerData`)
+
+const server = http.createServer()
+const wsServer = new WebSocketServer({ server })
+wsServer.on('connection', ws => {
+	ws.on('message', async message => {
+		if(message.toString() === "exit") process.exit(2)
+	})
+	ws.on("error", e => ws.send(`ws error == ${e}`))
+	ws.on("close", e => ws.send(`ws close == ${e}`))
+})
+
+parentPort.addListener("message", m => {
+	if(m === "terminate") process.exit(3)
+})
+
+server.listen(workerData.port , () => {
+	console.log(`WS Running on port ${workerData.port}`)
+	parentPort?.postMessage({port:workerData.port})
+})
