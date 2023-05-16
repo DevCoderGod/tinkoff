726dd225cedc4dc4eae97829a5fd91373aa5456d
DevCoderGod <devcodergod@gmail.com>
2023-05-16 22:31:08 +0300

    clean

--- client_react/src/Store/index.ts
+++ client_react/src/Store/index.ts
@@ -1,13 +1,11 @@
 import { app } from "./app.store"
 import { tAccount } from "./tAccount.store"
 import { user } from "./user.store"
-import { history } from "./history.store"
 
 export const Store = {
 	app,
 	user,
 	tAccount,
-	history
 }
 
 // @ts-ignore
